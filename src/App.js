10e929495e5a058e6e60fae696ae1f55109b98ff
blakekrammes <blakekrammes@protonmail.com>
2024-06-28 13:12:01 -0500

    Fetch house sales data
    - Remove CRA boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './fetch';
+
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/sales');
+      setData(res);
+    };
+    fetchData().catch((e) => console.error(e));
+  }, []);
+
+  if (data) console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
