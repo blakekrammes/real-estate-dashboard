93c0be826a64f21815d58ddb275d7ee9b8b732c1
blakekrammes <blakekrammes@protonmail.com>
2024-06-29 22:12:47 -0500

    Tweak dropdown menu
    - Make dropdown title descriptive
    - Center dropdown items below dropdown button

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -8,13 +8,12 @@ const Dropdown = ({ items, setSelectedTown }) => {
     <div className='fixed top-24 w-52 text-right'>
       <Menu>
         <MenuButton className='inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white'>
-          Options
+          Towns
           <ChevronDownIcon className='size-4 fill-white/60' />
         </MenuButton>
-
         <MenuItems
           transition
-          anchor='bottom end'
+          anchor='bottom'
           className='w-52 origin-top-right rounded-xl border border-gray-800 bg-gray-800 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0'
         >
           {items && items.length > 0
